929b181b09200c7bf8b0622c7756170357cefe0a
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2024-12-14 11:18:55 +0000

    made a simple pipeline

---
+++ models/mart_policy_renewal_metrics.sql
@@ -0,0 +1,34 @@
+WITH policy_history AS (
+    SELECT * FROM {{ ref('moh_demo', 'dim_policy_customer_history') }}
+),
+
+renewal_criteria AS (
+    SELECT * FROM {{ ref('renewal_rates') }}
+),
+
+policy_tenures AS (
+    SELECT 
+        policy_id,
+        policy_type,
+        customer_name,
+        state,
+        MIN(valid_from) as first_policy_date,
+        DATEDIFF('month', MIN(valid_from), CURRENT_DATE()) as tenure_months,
+        premium_amount
+    FROM policy_history
+    WHERE is_current = true
+    GROUP BY 1,2,3,4,6
+)
+
+SELECT 
+    pt.*,
+    rr.renewal_rate_percentage,
+    CASE 
+        WHEN pt.tenure_months >= rr.min_tenure_months THEN true 
+        ELSE false 
+    END as eligible_for_renewal,
+    DATEADD('month', 12, pt.first_policy_date) as next_renewal_date,
+    pt.premium_amount * (1 + (rr.renewal_rate_percentage/100)) as projected_renewal_amount
+FROM policy_tenures pt
+JOIN renewal_criteria rr 
+    ON pt.policy_type = rr.policy_type
